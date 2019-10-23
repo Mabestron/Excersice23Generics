4692a55b6d1abd38dc624e1569bb2f98d4d2203f
Mabestron <ivangamaliel555@hotmail.com>
2019-10-23 15:37:54 -0500

    Generic v2

--- src/com/Excersice23generics/app/GenericsApp.java
+++ src/com/Excersice23generics/app/GenericsApp.java
@@ -1,10 +1,27 @@
 package com.Excersice23generics.app;
 
-public class GenericsApp {
+import com.Excersice23generics.model.Suma;
+import com.Excersice23generics.model.Resta;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class GenericsApp
+{
+	public static void main(String[] args) 
+	{
+		Suma<Integer> miObjeto =new Suma <Integer>(15);
+		Suma<String> miObjetoString =new Suma <String>("Testo");
+		Resta<Integer,Integer> miObjetoResta =new Resta <Integer,Integer>(10,20);
+		Resta<Integer,Integer> miObjetoResta2 =new Resta <Integer,Integer>();
+		
+		System.out.println("Objeto de miObjeto:"+miObjeto.getMiObjeto());
+		System.out.println("Objeto de miObjetoString:"+miObjetoString.getMiObjeto());
+		
+		miObjetoResta2.setKey(100);
+		miObjetoResta2.setValue(200);
+		System.out.println("Objeto Resta1:"+miObjetoResta.ToString());
+		System.out.println("Objeto Resta2:"+miObjetoResta2.ToString());
+		
 	}
 
 }
---
+++ src/com/Excersice23generics/model/Suma.java
@@ -0,0 +1,19 @@
+package com.Excersice23generics.model;
+
+public class Suma <T>
+{
+	
+	T miObjeto;
+	
+	public Suma(T miObjeto)
+	{
+		this.miObjeto=miObjeto;
+	}
+	
+	public T  getMiObjeto()
+	{
+		return this.miObjeto;
+	}
+	
+	
+}
